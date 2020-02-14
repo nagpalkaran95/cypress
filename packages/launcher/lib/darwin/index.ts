c1631d6d01f8c7e545730774926c3eeecdc2d5a6
nagpalkaran95 <nagpalkaran95@gmail.com>
2020-02-14 18:29:46 +0530

    multiple ver fix for darwin

--- packages/launcher/lib/darwin/index.ts
+++ packages/launcher/lib/darwin/index.ts
@@ -5,121 +5,6 @@ import { log } from '../log'
 import { merge } from 'ramda'
 import { get } from 'lodash'
 
-// const detectChrome66 = partial(findApp, ['GoogleChrome66.app', 'Contents/MacOS/Google Chrome', 'com.google.Chrome', 'KSVersion'])
-// const detectChrome67 = partial(findApp, ['GoogleChrome67.app', 'Contents/MacOS/Google Chrome', 'com.google.Chrome', 'KSVersion'])
-// const detectChrome68 = partial(findApp, ['GoogleChrome68.app', 'Contents/MacOS/Google Chrome', 'com.google.Chrome', 'KSVersion'])
-// const detectChrome69 = partial(findApp, ['GoogleChrome69.app', 'Contents/MacOS/Google Chrome', 'com.google.Chrome', 'KSVersion'])
-// const detectChrome70 = partial(findApp, ['GoogleChrome70.app', 'Contents/MacOS/Google Chrome', 'com.google.Chrome', 'KSVersion'])
-// const detectChrome71 = partial(findApp, ['GoogleChrome71.app', 'Contents/MacOS/Google Chrome', 'com.google.Chrome', 'KSVersion'])
-// const detectChrome72 = partial(findApp, ['GoogleChrome72.app', 'Contents/MacOS/Google Chrome', 'com.google.Chrome', 'KSVersion'])
-// const detectChrome73 = partial(findApp, ['GoogleChrome73.app', 'Contents/MacOS/Google Chrome', 'com.google.Chrome', 'KSVersion'])
-// const detectChrome74 = partial(findApp, ['GoogleChrome74.app', 'Contents/MacOS/Google Chrome', 'com.google.Chrome', 'KSVersion'])
-// const detectChrome75 = partial(findApp, ['GoogleChrome75.app', 'Contents/MacOS/Google Chrome', 'com.google.Chrome', 'KSVersion'])
-// const detectChrome76 = partial(findApp, ['GoogleChrome76.app', 'Contents/MacOS/Google Chrome', 'com.google.Chrome', 'KSVersion'])
-// const detectChrome77 = partial(findApp, ['GoogleChrome77.app', 'Contents/MacOS/Google Chrome', 'com.google.Chrome', 'KSVersion'])
-// const detectChrome78 = partial(findApp, ['GoogleChrome78.app', 'Contents/MacOS/Google Chrome', 'com.google.Chrome', 'KSVersion'])
-// const detectChrome79 = partial(findApp, ['GoogleChrome79.app', 'Contents/MacOS/Google Chrome', 'com.google.Chrome', 'KSVersion'])
-
-// const detectFirefox60 = partial(findApp, ['firefox 60.0.app', 'Contents/MacOS/firefox-bin', 'org.mozilla.firefox', 'CFBundleShortVersionString'])
-// const detectFirefox61 = partial(findApp, ['firefox 61.0.app', 'Contents/MacOS/firefox-bin', 'org.mozilla.firefox', 'CFBundleShortVersionString'])
-// const detectFirefox62 = partial(findApp, ['firefox 62.0.app', 'Contents/MacOS/firefox-bin', 'org.mozilla.firefox', 'CFBundleShortVersionString'])
-// const detectFirefox63 = partial(findApp, ['firefox 63.0.app', 'Contents/MacOS/firefox-bin', 'org.mozilla.firefox', 'CFBundleShortVersionString'])
-// const detectFirefox64 = partial(findApp, ['firefox 64.0.app', 'Contents/MacOS/firefox-bin', 'org.mozilla.firefox', 'CFBundleShortVersionString'])
-// const detectFirefox65 = partial(findApp, ['firefox 65.0.app', 'Contents/MacOS/firefox-bin', 'org.mozilla.firefox', 'CFBundleShortVersionString'])
-// const detectFirefox66 = partial(findApp, ['firefox 66.0.app', 'Contents/MacOS/firefox-bin', 'org.mozilla.firefox', 'CFBundleShortVersionString'])
-// const detectFirefox67 = partial(findApp, ['firefox 67.0.app', 'Contents/MacOS/firefox-bin', 'org.mozilla.firefox', 'CFBundleShortVersionString'])
-// const detectFirefox68 = partial(findApp, ['firefox 68.0.app', 'Contents/MacOS/firefox-bin', 'org.mozilla.firefox', 'CFBundleShortVersionString'])
-// const detectFirefox69 = partial(findApp, ['firefox 69.0.app', 'Contents/MacOS/firefox-bin', 'org.mozilla.firefox', 'CFBundleShortVersionString'])
-// const detectFirefox70 = partial(findApp, ['firefox 70.0.app', 'Contents/MacOS/firefox-bin', 'org.mozilla.firefox', 'CFBundleShortVersionString'])
-// const detectFirefox71 = partial(findApp, ['firefox 71.0.app', 'Contents/MacOS/firefox-bin', 'org.mozilla.firefox', 'CFBundleShortVersionString'])
-// const detectFirefox72 = partial(findApp, ['firefox 72.0.app', 'Contents/MacOS/firefox-bin', 'org.mozilla.firefox', 'CFBundleShortVersionString'])
-
-// const detectOpera50 = partial(findApp, ['Opera 50.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-// const detectOpera51 = partial(findApp, ['Opera 51.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-// const detectOpera52 = partial(findApp, ['Opera 52.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-// const detectOpera53 = partial(findApp, ['Opera 53.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-// const detectOpera54 = partial(findApp, ['Opera 54.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-// const detectOpera55 = partial(findApp, ['Opera 55.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-// const detectOpera56 = partial(findApp, ['Opera 56.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-// const detectOpera57 = partial(findApp, ['Opera 57.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-// const detectOpera58 = partial(findApp, ['Opera 58.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-// const detectOpera59 = partial(findApp, ['Opera 59.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-// const detectOpera60 = partial(findApp, ['Opera 60.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-// const detectOpera61 = partial(findApp, ['Opera 61.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-// const detectOpera62 = partial(findApp, ['Opera 62.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-// const detectOpera63 = partial(findApp, ['Opera 63.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-// const detectOpera64 = partial(findApp, ['Opera 64.0.app', 'Contents/MacOS/Opera', 'com.operasoftware.Opera', 'CFBundleShortVersionString'])
-
-// const detectEdge79 = partial(findApp, ['Edge79.app', 'Contents/MacOS/Microsoft Edge', 'com.microsoft.Edge', 'CFBundleShortVersionString'])
-// const detectEdge80 = partial(findApp, ['Edge80.app', 'Contents/MacOS/Microsoft Edge', 'com.microsoft.Edge', 'CFBundleShortVersionString'])
-
-// const detectEdge79Beta = partial(findApp, ['Edgebeta79.app', 'Contents/MacOS/Microsoft Edge Beta', 'com.microsoft.Edge', 'CFBundleShortVersionString'])
-// const detectEdge80Beta = partial(findApp, ['Edgebeta80.app', 'Contents/MacOS/Microsoft Edge Beta', 'com.microsoft.Edge', 'CFBundleShortVersionString'])
-
-// const detectEdge81Dev = partial(findApp, ['Edgedev81.app', 'Contents/MacOS/Microsoft Edge Dev', 'com.microsoft.Edge', 'CFBundleShortVersionString'])
-
-// // to detect browsers on your local system
-// const detectCanary = partial(findApp, [
-//   'Google Chrome Canary.app',
-//   'Contents/MacOS/Google Chrome Canary',
-//   'com.google.Chrome.canary',
-//   'KSVersion',
-// ])
-// const detectChrome = partial(findApp, [
-//   'Google Chrome.app',
-//   'Contents/MacOS/Google Chrome',
-//   'com.google.Chrome',
-//   'KSVersion',
-// ])
-// const detectChromium = partial(findApp, [
-//   'Chromium.app',
-//   'Contents/MacOS/Chromium',
-//   'org.chromium.Chromium',
-//   'CFBundleShortVersionString',
-// ])
-// const detectFirefox = partial(findApp, [
-//   'Firefox.app',
-//   'Contents/MacOS/firefox-bin',
-//   'org.mozilla.firefox',
-//   'CFBundleShortVersionString',
-// ])
-// const detectFirefoxDeveloperEdition = partial(findApp, [
-//   'Firefox Developer Edition.app',
-//   'Contents/MacOS/firefox-bin',
-//   'org.mozilla.firefoxdeveloperedition',
-//   'CFBundleShortVersionString',
-// ])
-// const detectFirefoxNightly = partial(findApp, [
-//   'Firefox Nightly.app',
-//   'Contents/MacOS/firefox-bin',
-//   'org.mozilla.nightly',
-//   'CFBundleShortVersionString',
-// ])
-// const detectEdgeCanary = partial(findApp, [
-//   'Microsoft Edge Canary.app',
-//   'Contents/MacOS/Microsoft Edge Canary',
-//   'com.microsoft.Edge.Canary',
-//   'CFBundleShortVersionString',
-// ])
-// const detectEdgeBeta = partial(findApp, [
-//   'Microsoft Edge Beta.app',
-//   'Contents/MacOS/Microsoft Edge Beta',
-//   'com.microsoft.Edge.Beta',
-//   'CFBundleShortVersionString',
-// ])
-// const detectEdgeDev = partial(findApp, [
-//   'Microsoft Edge Dev.app',
-//   'Contents/MacOS/Microsoft Edge Dev',
-//   'com.microsoft.Edge.Dev',
-//   'CFBundleShortVersionString',
-// ])
-// const detectEdge = partial(findApp, [
-//   'Microsoft Edge.app',
-//   'Contents/MacOS/Microsoft Edge',
-//   'com.microsoft.Edge',
-//   'CFBundleShortVersionString',
-// ])
-
 type Detectors = {
   [name: string]: {
     [channel: string]: FindAppParams
@@ -195,6 +80,55 @@ export const browsers: Detectors = {
       versionProperty: 'CFBundleShortVersionString',
     },
   },
+  chrome66: { stable: { appName: 'GoogleChrome66.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  chrome67: { stable: { appName: 'GoogleChrome67.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  chrome68: { stable: { appName: 'GoogleChrome68.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  chrome69: { stable: { appName: 'GoogleChrome69.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  chrome70: { stable: { appName: 'GoogleChrome70.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  chrome71: { stable: { appName: 'GoogleChrome71.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  chrome72: { stable: { appName: 'GoogleChrome72.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  chrome73: { stable: { appName: 'GoogleChrome73.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  chrome74: { stable: { appName: 'GoogleChrome74.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  chrome75: { stable: { appName: 'GoogleChrome75.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  chrome76: { stable: { appName: 'GoogleChrome76.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  chrome77: { stable: { appName: 'GoogleChrome77.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  chrome78: { stable: { appName: 'GoogleChrome78.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  chrome79: { stable: { appName: 'GoogleChrome79.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  chrome80: { stable: { appName: 'GoogleChrome80.app', executable: 'Contents/MacOS/Google Chrome', appId: 'com.google.Chrome', versionProperty: 'KSVersion' } },
+  firefox60: { stable: { appName: 'firefox 60.0.app', executable: 'Contents/MacOS/firefox-bin', appId: 'org.mozilla.firefox', versionProperty: 'CFBundleShortVersionString' } },
+  firefox61: { stable: { appName: 'firefox 61.0.app', executable: 'Contents/MacOS/firefox-bin', appId: 'org.mozilla.firefox', versionProperty: 'CFBundleShortVersionString' } },
+  firefox62: { stable: { appName: 'firefox 62.0.app', executable: 'Contents/MacOS/firefox-bin', appId: 'org.mozilla.firefox', versionProperty: 'CFBundleShortVersionString' } },
+  firefox63: { stable: { appName: 'firefox 63.0.app', executable: 'Contents/MacOS/firefox-bin', appId: 'org.mozilla.firefox', versionProperty: 'CFBundleShortVersionString' } },
+  firefox64: { stable: { appName: 'firefox 64.0.app', executable: 'Contents/MacOS/firefox-bin', appId: 'org.mozilla.firefox', versionProperty: 'CFBundleShortVersionString' } },
+  firefox65: { stable: { appName: 'firefox 65.0.app', executable: 'Contents/MacOS/firefox-bin', appId: 'org.mozilla.firefox', versionProperty: 'CFBundleShortVersionString' } },
+  firefox66: { stable: { appName: 'firefox 66.0.app', executable: 'Contents/MacOS/firefox-bin', appId: 'org.mozilla.firefox', versionProperty: 'CFBundleShortVersionString' } },
+  firefox67: { stable: { appName: 'firefox 67.0.app', executable: 'Contents/MacOS/firefox-bin', appId: 'org.mozilla.firefox', versionProperty: 'CFBundleShortVersionString' } },
+  firefox68: { stable: { appName: 'firefox 68.0.app', executable: 'Contents/MacOS/firefox-bin', appId: 'org.mozilla.firefox', versionProperty: 'CFBundleShortVersionString' } },
+  firefox69: { stable: { appName: 'firefox 69.0.app', executable: 'Contents/MacOS/firefox-bin', appId: 'org.mozilla.firefox', versionProperty: 'CFBundleShortVersionString' } },
+  firefox70: { stable: { appName: 'firefox 70.0.app', executable: 'Contents/MacOS/firefox-bin', appId: 'org.mozilla.firefox', versionProperty: 'CFBundleShortVersionString' } },
+  firefox71: { stable: { appName: 'firefox 71.0.app', executable: 'Contents/MacOS/firefox-bin', appId: 'org.mozilla.firefox', versionProperty: 'CFBundleShortVersionString' } },
+  firefox72: { stable: { appName: 'firefox 72.0.app', executable: 'Contents/MacOS/firefox-bin', appId: 'org.mozilla.firefox', versionProperty: 'CFBundleShortVersionString' } },
+  firefox73: { stable: { appName: 'firefox 73.0.app', executable: 'Contents/MacOS/firefox-bin', appId: 'org.mozilla.firefox', versionProperty: 'CFBundleShortVersionString' } },
+  opera50: { stable: { appName: 'Opera 50.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  opera51: { stable: { appName: 'Opera 51.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  opera52: { stable: { appName: 'Opera 52.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  opera53: { stable: { appName: 'Opera 53.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  opera54: { stable: { appName: 'Opera 54.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  opera55: { stable: { appName: 'Opera 55.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  opera56: { stable: { appName: 'Opera 56.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  opera57: { stable: { appName: 'Opera 57.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  opera58: { stable: { appName: 'Opera 58.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  opera59: { stable: { appName: 'Opera 59.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  opera60: { stable: { appName: 'Opera 60.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  opera61: { stable: { appName: 'Opera 61.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  opera62: { stable: { appName: 'Opera 62.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  opera63: { stable: { appName: 'Opera 63.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  opera64: { stable: { appName: 'Opera 64.0.app', executable: 'Contents/MacOS/Opera', appId: 'com.operasoftware.Opera', versionProperty: 'CFBundleShortVersionString' } },
+  edge79: { stable: { appName: 'Edge79.app', executable: 'Contents/MacOS/Microsoft Edge', appId: 'com.microsoft.Edge', versionProperty: 'CFBundleShortVersionString' } },
+  edge80: { stable: { appName: 'Edge80.app', executable: 'Contents/MacOS/Microsoft Edge', appId: 'com.microsoft.Edge', versionProperty: 'CFBundleShortVersionString' } },
+  edge79beta: { stable: { appName: 'Edgebeta79.app', executable: 'Contents/MacOS/Microsoft Edge Beta', appId: 'com.microsoft.Edge', versionProperty: 'CFBundleShortVersionString' } },
+  edge80beta: { stable: { appName: 'Edgebeta80.app', executable: 'Contents/MacOS/Microsoft Edge Beta', appId: 'com.microsoft.Edge', versionProperty: 'CFBundleShortVersionString' } },
+  edge81dev: { stable: { appName: 'Edgedev81.app', executable: 'Contents/MacOS/Microsoft Edge Dev', appId: 'com.microsoft.Edge', versionProperty: 'CFBundleShortVersionString' } },
 }
 
 export const getVersionString = linuxHelper.getVersionString
